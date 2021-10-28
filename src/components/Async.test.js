a26d7c851b661763f95968cfc9bf17467b40dd2d
Yang <qiaoyanandyang@gmail.com>
2021-10-28 17:20:03 +0800

    Testing Asynchronous Code

---
+++ src/components/Async.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import Async from "./Async";
+describe("Async component", () => {
+  test("renders posts if request succeeds", async() => {
+    // Arrange
+    render(<Async />);
+
+    // Act
+    // nothing
+
+    // Assert
+    const listItemElements = await screen.findAllByRole("listitem");
+    expect(listItemElements).not.toHaveLength(0);
+  });
+});
